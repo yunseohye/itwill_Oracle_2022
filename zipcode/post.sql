91c457fc1677136af411e2beb695abbb364e7ac0
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-07-22 08:39:10 +0900

    Create post.sql

---
+++ zipcode/post.sql
@@ -0,0 +1,9 @@
+CREATE TABLE post (
+	seq  NUMBER  NOT NULL
+	,zipcode  VARCHAR2(10)
+	,sido  VARCHAR2(50)
+	,gugun  VARCHAR2(100)
+	,dong  VARCHAR2(100)
+	,bunji  VARCHAR2(50)
+        ,PRIMARY KEY (seq)
+);
